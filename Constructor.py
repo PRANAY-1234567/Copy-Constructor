bf8bf98c3c368674967d7deba26b80577997db9e
Pranay <pranayvjadhao197@gmail.com>
2026-04-16 10:38:02 +0530

    Initial commit

---
+++ Constructor.py
@@ -0,0 +1,25 @@
+class Sample:
+    def __init__(self, x=None, y=None, z=None):
+        if isinstance(x, Sample):
+            t = x
+            self.x = t.x + 5
+            self.y = t.y + 5
+            self.z = t.z + 5
+
+        else:
+            self.x = x
+            self.y = y
+            self.z = z
+
+    def display(self):
+        print(f"xx = {self.x}\ty = {self.y}\tz = {self.z}")
+
+
+
+s1 = Sample(53, 66, 68)
+s2 = Sample(s1)   # copy constructor
+s3 = Sample(s2)   # copy constructor again
+
+s1.display()
+s2.display()
+s3.display()
